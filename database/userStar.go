b2251e6a366f037f5638b7a9fb6f91d24d4c60c4
jjmin321 <jjmin321@naver.com>
2020-09-08 14:20:20 +0900

    [MODIFY] database/userStar.go

--- database/userStar.go
+++ database/userStar.go
@@ -5,5 +5,5 @@ type UserStar struct {
 	Idx         uint   `gorm:"primary_key; auto_increment:true" json:"idx"`
 	FkObjectIdx uint   `gorm:"not null;" json:"fk_object_idx"`
 	Star        uint   `gorm:"not null" json:"star"`
-	FkUserID    string `gorm:"not null;" json:"fk_user_id"`
+	FkUserID    string `gorm:"type:varchar(255);not null" json:"fk_user_id"`
 }
