e975e95e337165b0161def118c459e64bff4fd2b
jjmin321 <jjmin321@naver.com>
2020-09-08 14:13:38 +0900

    [MODIFY] Primary_key In UserStar.go

--- database/userStar.go
+++ database/userStar.go
@@ -2,7 +2,8 @@ package database
 
 // UserStar 유저 평가 테이블
 type UserStar struct {
-	FkObjectIdx uint `gorm:"primary_key;" json:"fk_object_idx"`
+	Idx         uint `gorm:"primary_key; auto_increment:true" json:"idx"`
+	FkObjectIdx uint `gorm:"not null;" json:"fk_object_idx"`
 	Star        uint `gorm:"not null" json:"star"`
 	FkUserID    uint `gorm:"not null;" json:"fk_user_id"`
 }
