6df4b96b67ccade2a1acd97298c38a5f127ba1be
jjmin321 <jjmin321@naver.com>
2020-09-08 17:46:26 +0900

    [UPDATE] controller/updateComment.go

--- controller/updateComment.go
+++ controller/updateComment.go
@@ -1,6 +1,10 @@
 package controller
 
-import "github.com/labstack/echo"
+import (
+	"Amanda_Server/database"
+
+	"github.com/labstack/echo"
+)
 
 // UpdateCommentParam - 파라미터 형식 정의 구조체
 type UpdateCommentParam struct {
@@ -14,8 +18,17 @@ func UpdateComment(c echo.Context) error {
 	if err := c.Bind(u); err != nil {
 		return err
 	}
+	ID := c.Get("ID").(string)
+	UserComment := &database.UserComment{}
+	err := database.DB.Model(UserComment).Update("comment", u.Comment).Where("idx = ?", u.Idx).Error
+	if err != nil {
+		return c.JSON(500, map[string]interface{}{
+			"status":  500,
+			"message": "댓글 수정 실패",
+		})
+	}
 	return c.JSON(200, map[string]interface{}{
 		"status":  200,
-		"message": "댓글 달기 성공",
+		"message": "댓글 수정 성공",
 	})
 }
