e8a913a965271b5b5b3b5a6b495a90adbd064acc
jjmin321 <jjmin321@naver.com>
2020-09-08 16:47:10 +0900

    [UPDATE] controller/createComment.go

---
+++ controller/createComment.go
@@ -0,0 +1,34 @@
+package controller
+
+import (
+	"Amanda_Server/database"
+
+	"github.com/labstack/echo"
+)
+
+// CreateCommentParam - 파라미터 형식 정의 구조체
+type CreateCommentParam struct {
+	Idx     uint   `json:"idx" form:"idx" query:"idx"`
+	Comment string `json:"comment" form:"comment" query:"comment"`
+}
+
+// CreateComment - 댓글을 작성하는 메서드
+func CreateComment(c echo.Context) error {
+	ID := c.Get("ID").(string)
+	u := new(CreateCommentParam)
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	UserComment := &database.UserComment{FkUserID: ID, FkObjectIdx: u.Idx, Comment: u.Comment}
+	err := database.DB.Create(UserComment).Error
+	if err != nil {
+		return c.JSON(500, map[string]interface{}{
+			"status":  500,
+			"message": "댓글 달기 실패",
+		})
+	}
+	return c.JSON(200, map[string]interface{}{
+		"status":  200,
+		"message": "댓글 달기 성공",
+	})
+}
--- controller/updateComment.go
+++ controller/updateComment.go
@@ -4,13 +4,17 @@ import "github.com/labstack/echo"
 
 // UpdateCommentParam - 파라미터 형식 정의 구조체
 type UpdateCommentParam struct {
-	Idx  uint `json:"idx" form:"idx" query:"idx"`
-	Star uint `json:"star" form:"star" query:"star"`
+	Idx     uint   `json:"idx" form:"idx" query:"idx"`
+	Comment string `json:"comment" form:"comment" query:"comment"`
 }
 
-func UpdateComment(c echo.Context) {
-	u := new(UpdateStarParam)
+func UpdateComment(c echo.Context) error {
+	u := new(UpdateCommentParam)
 	if err := c.Bind(u); err != nil {
 		return err
 	}
+	return c.JSON(200, map[string]interface{}{
+		"status":  200,
+		"message": "댓글 달기 성공",
+	})
 }
