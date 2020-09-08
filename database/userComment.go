6436f58fbec37911a7f3e280bc1fa3a152b6d1e7
jjmin321 <jjmin321@naver.com>
2020-09-08 17:12:58 +0900

    [MODIFY] database/userComment.go

--- database/userComment.go
+++ database/userComment.go
@@ -4,9 +4,9 @@ import "time"
 
 // UserComment 평가하는 댓글 테이블
 type UserComment struct {
-	Idx         uint      `gorm:"primary_key; auto_increment:true" json:"idx"`
-	FkUserID    string    `gorm:"type:varchar(255);not null" json:"fk_user_id"`
-	FkObjectIdx uint      `gorm:"not null" json:"fk_object_idx"`
-	Comment     string    `gorm:"type:varchar(255);not null" json:"comment"`
-	CreatedAt   time.Time `gorm:"not null" sql:"DEFAULT:current_timestamp" json:"created_at"`
+	Idx        uint      `gorm:"primary_key; auto_increment:true" json:"idx"`
+	FkUserID   string    `gorm:"type:varchar(255);not null" json:"fk_user_id"`
+	FkObjectID string    `gorm:"type:varchar(255);not null" json:"fk_object_id"`
+	Comment    string    `gorm:"type:varchar(255);not null" json:"comment"`
+	CreatedAt  time.Time `gorm:"not null" sql:"DEFAULT:current_timestamp" json:"created_at"`
 }
